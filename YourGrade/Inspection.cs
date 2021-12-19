6376569c288e56cec09975ca598deedb6e0c6b10
Voyah <104687991+Voyah@users.noreply.github.com>
2021-12-19 18:59:13 +0000

    Added new icon

--- YourGrade/Inspection.cs
+++ YourGrade/Inspection.cs
@@ -52,3 +52,64 @@ namespace YourGrade
             if (word == "F")
                 return true;
             return false;
+        }
+
+        public static bool IsUnivPoint(string word)
+        {
+            string pattern = "\\d{1}";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsSeparatedGrade(string word)
+        {
+            string pattern = "^[A-D]$";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsGrade(string word)
+        {
+            string pattern = "^[A-D][+|-|0]$";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsDate(string word)
+        {
+            string pattern = "\\d{4}[?][1|2|여름|겨울]";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsRetaked(string word)
+        {
+            string pattern = "^Ex2+";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsFGrade(string word)
+        {
+            string pattern = "^Ex3+";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsDuplicated(string word)
+        {
+            string pattern = "^Ex5+";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsAbandoned(string word)
+        {
+            string pattern = "^Ex6+";
+            return Regex.IsMatch(word, pattern);
+        }
+
+        public static bool IsRetakedSubjectCode(string word, List<Subject> list)
+        {
+            foreach(Subject sub in list)
+            {
+                if (word == sub.Code)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
