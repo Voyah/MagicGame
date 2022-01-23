03eb58df04561a91e24127c9099393f63b75b947
Voyah <104687991+Voyah@users.noreply.github.com>
2022-01-23 06:00:58 +0000

    Bump hosted-git-info from 2.8.5 to 2.8.9

--- YourGrade/Properties/Settings.Designer.cs
+++ YourGrade/Properties/Settings.Designer.cs
@@ -13,4 +13,5 @@ namespace YourGrade.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.Settin
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed
