0c00d126adbbb04f29a7468a3e3ce1bbb48c5456
Voyah <104687991+Voyah@users.noreply.github.com>
2022-01-23 09:43:24 +0000

    Pontoon: Update Italian (it) localization of Common Voice

--- YourGrade/Properties/Settings.Designer.cs
+++ YourGrade/Properties/Settings.Designer.cs
@@ -14,4 +14,5 @@ namespace YourGrade.Properties
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+  
