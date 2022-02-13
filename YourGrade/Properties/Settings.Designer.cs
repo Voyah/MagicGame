5e8ddd16df5716693bf0cf6c584ecb3f9a010286
Voyah <104687991+Voyah@users.noreply.github.com>
2022-02-13 17:15:51 +0000

    Fast-forwarding changes

--- YourGrade/Properties/Settings.Designer.cs
+++ YourGrade/Properties/Settings.Designer.cs
@@ -17,4 +17,8 @@ namespace YourGrade.Properties
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
 
-        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings()
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+
+        public static Settings Default
+        {
+     
