34e97a606066a7253a1d4d96ee424608fd91f942
Voyah <104687991+Voyah@users.noreply.github.com>
2022-03-13 17:45:10 +0000

    Sanity checks

--- YourGrade/Subject.cs
+++ YourGrade/Subject.cs
@@ -9,4 +9,4 @@ namespace YourGrade
     public class Subject
     {
         public static readonly string[] REFINEMENT_CLASS = { "교양필수", "교양선택", "일반선택" };
-  
+        public static readonly string[] MAJOR_CLASS = { "전공기초", "전공필수", "전공선택", 
