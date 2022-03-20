ab50bcdf56a3e02fa053ccac316c74bc5f477684
Voyah <104687991+Voyah@users.noreply.github.com>
2022-03-20 10:16:26 +0000

    :arrow_up: 1.1.8 :tada:

--- YourGrade/Subject.cs
+++ YourGrade/Subject.cs
@@ -10,4 +10,7 @@ namespace YourGrade
     {
         public static readonly string[] REFINEMENT_CLASS = { "교양필수", "교양선택", "일반선택" };
         public static readonly string[] MAJOR_CLASS = { "전공기초", "전공필수", "전공선택", "융합선택", "융합필수", "복수전공" };
-        public static readonly string IGNORE = "1001
+        public static readonly string IGNORE = "1001";
+
+        private string classification;
+        private strin
